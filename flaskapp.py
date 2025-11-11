bb5eec2b7b35bcf602f97d50e9818b1b4766c07c
Diego Campos <126905708+diegocampos3@users.noreply.github.com>
2025-11-11 06:38:23 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -43,3 +44,38 @@ def analyze():
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import numpy as np
+
+# Cargar modelos
+loaded_model = joblib.load("./pkl_objects/model2.pkl")
+loaded_vec = joblib.load("./pkl_objects/vectorizer2.pkl")
+
+app = Flask(__name__)
+CORS(app)  # Permitir peticiones externas
+
+def classify(document):
+    label = {0: 'negativo', 1: 'positivo'}
+    X = loaded_vec.transform([document])
+    y = loaded_model.predict(X)[0]
+    proba = np.max(loaded_model.predict_proba(X))
+    return label[y], proba
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    if request.is_json:
+        data = request.get_json()
+        review = data.get('comment')
+        if not review:
+            return jsonify({'error': 'Falta el campo "comment" en el JSON'}), 400
+        prediction, probability = classify(review)
+        return jsonify({'comment': review, 'prediction': prediction})
+    else:
+        return jsonify({'error': 'La solicitud debe ser en formato JSON'}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
